50df71fb28b62ffd5b8075e2e96a9535677b3aa7
yu <yul1024@outlook.com>
2026-04-20 18:39:46 +0800

    feat: add data frame converter

--- data_table_conversion/df_converter.py
+++ data_table_conversion/df_converter.py
@@ -16,6 +16,7 @@ from __future__ import annotations
 from loguru import logger
 
 import pandas as pd
+from pathlib import Path
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
@@ -47,5 +48,30 @@ class DataFrameConverter:
             )
         )
         logger.info(f"\nLaTeX output:\n{latex_output}")
+        return latex_output
+
+    @staticmethod
+    def convert_and_save_df(
+        df: pd.DataFrame,
+        precision: int,
+        label: str,
+        caption: str,
+        position: str,
+        result_path: str,
+    ) -> str:
+        # path processing
+        result_path = Path(result_path)
+        result_path.parent.mkdir(parents=True, exist_ok=True)
+        # convert df
+        latex_output = DataFrameConverter.convert_df(
+            df=df,
+            precision=precision,
+            label=label,
+            caption=caption,
+            position=position,
+        )
+        # save latex table code
+        result_path.write_text(latex_output, encoding='utf-8')
+        logger.success(f"Saved latex table code to {result_path}")
         return latex_output
 
