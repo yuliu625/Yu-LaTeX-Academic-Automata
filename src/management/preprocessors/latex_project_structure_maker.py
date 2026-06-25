0cdda4b234f31497e726833d9d259060ad207168
yu <yul1024@outlook.com>
2026-06-25 19:26:45 +0800

    refactor: rename and move

---
+++ src/management/preprocessors/latex_project_structure_maker.py
@@ -0,0 +1,26 @@
+"""
+Sources:
+    https://github.com/yuliu625/Yu-LaTeX-Academic-Automata/blob/main/src/management/preprocessors/latex_project_structure_maker.py
+
+References:
+    https://github.com/yuliu625/Yu-LaTeX-Academic-Template
+
+Synopsis:
+    Make latex project structure via python script.
+
+Notes:
+    遵循通用定义的 latex project 的项目结构，以脚本化方法自动生成项目结构。
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class LaTeXProjectStructureMaker:
+    ...
+
