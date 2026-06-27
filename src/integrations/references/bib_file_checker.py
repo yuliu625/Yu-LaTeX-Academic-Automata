fac7527d6eb291461d71e527f126dbfc365854ca
yu <yul1024@outlook.com>
2026-06-27 16:39:40 +0800

    feat: add bib file checker

---
+++ src/integrations/references/bib_file_checker.py
@@ -0,0 +1,26 @@
+"""
+Sources:
+    https://github.com/yuliu625/Yu-LaTeX-Academic-Automata/blob/main/src/integrations/references/bib_file_checker.py
+
+References:
+    https://bibtexparser.readthedocs.io/en/main/
+
+Synopsis:
+    Check `.bib` file for bibliographic references.
+
+Notes:
+
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+import bibtexparser
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class BibFileChecker:
+    raise NotImplementedError
+
